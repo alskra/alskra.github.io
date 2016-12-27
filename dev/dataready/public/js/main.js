1c3241610ca218b3e010d4bde6f4c803ec618cd7
alskra <krasnoyurchenko.a.s@gmail.com>
2016-12-27 16:14:35 +0300

    upd

--- dev/dataready/public/js/main.js
+++ dev/dataready/public/js/main.js
@@ -169,7 +169,7 @@ function serviceItem() {
 $(function () {
     $(window).on('resize.serviceItem', function () {
         $('.service-item__img').each(function () {
-            $(this).css('max-height', $(this).closest('.service-item__col_1').height());
+            $(this).css('max-height', '').css('max-height', $(this).closest('.service-item__col_1').height());
         });
         $('.service-item__col_2').each(function () {
             $(this).trigger('destroy').dotdotdot();
